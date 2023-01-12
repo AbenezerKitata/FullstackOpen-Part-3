297c4578a89544e75a4d1585e843fd774794ebc9
abesunbuggy <abenezer@sunbuggyfunrentals.com>
2023-01-11 17:28:48 -0800

    done 3.12

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+const password = process.argv[2];
+const url = `mongodb+srv://aben4kit:${password}@cluster0.j2qmqfs.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+const Phonebook = mongoose.model("Phonebook", phonebookSchema);
+process.argv[3]
+  ? mongoose
+      .connect(url)
+      .then((result) => {
+        // console.log("connected");
+
+        const phonebook = new Phonebook({
+          name: process.argv[3],
+          number: process.argv[4],
+        });
+
+        return phonebook.save();
+      })
+      .then(() => {
+        console.log(
+          `added ${process.argv[3]} number ${process.argv[4]} to phonebook`
+        );
+        return mongoose.connection.close();
+      })
+      .catch((err) => console.log(err))
+  : mongoose.connect(url).then((result) => {
+      console.log("phonebook:");
+      Phonebook.find({}).then((result) => {
+        result.forEach((phonebook) => {
+          console.log(`${phonebook.name} ${phonebook.number}`);
+        });
+        mongoose.connection.close();
+      });
+    });
